595ff289328ed363e78be367a52f8b9d1db81f57
bridgetf7 <fbridget0404@gmail.com>
2023-05-10 13:07:12 -0400

    added shopping cart icon to nav bar :)

--- sql-project/src/components/NavBar.vue
+++ sql-project/src/components/NavBar.vue
@@ -1,7 +1,14 @@
 <template>
     <nav class="navTab">
         <router-link to="/">home</router-link>
+        <router-link alt="shopping-cart" to="/"><img src="/shopping-cart.svg" width="25" height="25" /></router-link>
     </nav>
 </template>
   
-<style scoped></style>
+<style scoped>
+.navTab {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+</style>
