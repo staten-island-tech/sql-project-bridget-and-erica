1b17e78a1a938d81a4ff5477d4050d00e814f338
bridgetf7 <fbridget0404@gmail.com>
2023-05-25 13:00:50 -0400

    product card file

---
+++ sql-project/src/components/ProductCard.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="productCard">
+        <h3>{{ product.name }}</h3>
+        <p class="productType">{{ product.type }}</p>
+        <p>{{ usDollar.format(product.price) }}</p>
+    </div>
+</template>
+  
+<script>
+
+export default {
+    name: "ProductCard",
+    props: {
+        product: Object,
+    },
+    components: {
+        AddButton
+    },
+    data() {
+        return {
+            usDollar: new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+            }),
+        }
+    },
+}
+</script>
+  
+<style scoped>
+template {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.productCard {
+    font-family: 'Cormorant Garamond', serif;
+    background-color: var(--lightGray);
+    margin: 1rem;
+    padding: 1.25rem;
+    display: inline-block;
+    width: 16rem;
+    height: 25rem;
+    line-height: 0.5rem;
+}
+
+img {
+    width: 16rem;
+}
+
+.productType {
+    color: black;
+    text-transform: capitalize;
+}
+
+hr {
+    display: block;
+    width: 80%;
+    background-color: black;
+    height: 0.02rem;
+}
+
+h2 {
+    line-height: 0;
+}
+</style>
--- sql-project/src/views/HomeView.vue
+++ sql-project/src/views/HomeView.vue
@@ -1,17 +1,18 @@
 <template>
   <main>
     <h1>home page :)</h1>
-
+    <ProductCard />
   </main>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from "../clients/supabase";
+import ProductCard from '../components/ProductCard.vue';
 
 async function getproducts() {
-  const { data } = await supabase.from('products').select()
-  console.log(data)
+  const { product } = await supabase.from('products').select()
+  console.log(product)
 }
 
 onMounted(() => {
