9df995a00e3d248f147fb227f76f5ed0068759b1
bridgetf7 <fbridget0404@gmail.com>
2023-05-25 12:53:43 -0400

    pulled data from sql

--- sql-project/src/views/HomeView.vue
+++ sql-project/src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup>
-</script>
-
 <template>
   <main>
     <h1>home page :)</h1>
 
   </main>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from "../clients/supabase";
+
+async function getproducts() {
+  const { data } = await supabase.from('products').select()
+  console.log(data)
+}
+
+onMounted(() => {
+  getproducts()
+})
+</script>
