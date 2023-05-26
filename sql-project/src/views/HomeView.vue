66afb98d4f2e346c433235ff4af71f7624e92580
ericazlin <113201249+ericazlin@users.noreply.github.com>
2023-05-26 13:11:55 -0400

    yuh

--- sql-project/src/views/HomeView.vue
+++ sql-project/src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <template>
   <main>
-    <div class="sideMenu">
-      <SideMenu />
+    <div class="homeView">
+      <div class="shingles">
+        <img src="/shingles.svg" height="233" width="1957" alt="">
+      </div>
+      <div class="groceries">
+        <h1 class="groceryText">
+          groceries :)
+        </h1>
+      </div>
+      <div class="homeContent">
+        <div class="sideMenu">
+          <SideMenu />
+        </div>
+        <div class="productCards">
+          <ProductCard v-for="product in products" :product="product" :key="product.id" />
+        </div>
+      </div>
+
     </div>
-    <ProductCard v-for="product in products" :product="product" :key="product.id" />
+
+
   </main>
 </template>
 
@@ -26,3 +43,35 @@ onMounted(() => {
 });
 </script>
 
+<style>
+.homeView {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  overflow: hidden;
+}
+
+.shingles {
+  width: 100%;
+  overflow: hidden;
+}
+
+.homeContent {
+  display: flex;
+  flex-direction: row;
+}
+
+.groceryText {
+  font-family: 'Montserrat', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 64px;
+  line-height: 78px;
+  border: 10px  #FBBA7D solid;
+  border-radius: 55% ;
+  text-align: center;
+  width: 798px;
+height: 304px;
+  color:  #FBBA7D;
+}
+</style>
